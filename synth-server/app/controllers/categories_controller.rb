75c0b71c7cbd95f6af54da28c058a2f9308fc516
Andrew Kloecker <lt.kvothe@gmail.com>
2020-07-23 16:33:57 -0400

    server work first pass

--- synth-server/app/controllers/categories_controller.rb
+++ synth-server/app/controllers/categories_controller.rb
@@ -1,2 +1,13 @@
 class CategoriesController < ApplicationController
+  def index
+    categories = Category.all
+    render json: categories.as_json(
+      only: [:name],
+      include: {
+        settings: {
+          only: [:name, :id]
+        }
+      }
+    )
+  end
 end
--- synth-server/app/controllers/settings_controller.rb
+++ synth-server/app/controllers/settings_controller.rb
@@ -1,2 +1,46 @@
 class SettingsController < ApplicationController
+  def index
+    settings = Setting.all
+    render json: settings.as_json(
+      only: [:id, :name, :gain, :stop_time, :a_frequency],
+      include: {
+        category: {
+          only: [:id, :name]
+        }
+      }
+    )
+  end
+
+  def create
+    setting = Setting.create(setting_params)
+    if setting.create
+      render json: setting.to_json(
+        except: [:created_at, :updated_at]
+      )
+    else
+      render json: { errors: setting.errors }
+    end
+  end
+
+  def update
+    setting = Setting.find(params[:id])
+    if setting.update(setting_params)
+      render json: setting.to_json(
+        except: [:created_at, :updated_at]
+      )
+    else
+      render json: { msg: "unable to update #{setting.name}" }
+    end
+  end
+
+  def destroy
+    setting = Setting.find(params[:id])
+    setting.destroy
+  end
+
+private
+
+  def setting_params
+    params.require(:setting).permit(:name, :gain, :stop_time, :a_frequency, :category_id)
+  end
 end
